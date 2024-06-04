666259fea86f5b419a07736b22c70176ce30e452
James Volmert <james.volmert@gmail.com>
2024-06-04 15:30:06 -0600

    got rid of node modules

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,4 @@
-const api_url = "http://localhost";
+const api_url = "http://localhost:8000";
 async function httpGetPlanets() {
   const res = await fetch(`${api_url}/planets`);
   return await res.json();

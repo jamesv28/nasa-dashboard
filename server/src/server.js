2fdc22c011a31b16a854c57609023b5d578977bc
James Volmert <james.volmert@gmail.com>
2024-06-04 17:23:56 -0600

    successfully get planets back end

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,15 @@
 const http = require("http");
 const app = require("./app");
 const PORT = process.env.PORT || 8000;
+const { loadPlanetsData } = require("./models/planets.model");
 
 const server = http.createServer(app);
-server.listen(PORT, () => {
-  console.log(`Port listening on: ${PORT}`);
-});
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Port listening on: ${PORT}`);
+  });
+}
+
+startServer();

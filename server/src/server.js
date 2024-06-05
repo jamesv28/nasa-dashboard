292a126c0771123c2938cf0434b8f4062202687d
James Volmert <james.volmert@gmail.com>
2024-06-04 18:32:50 -0600

    setup to run client and server together

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const app = require("./app");
 const PORT = process.env.PORT || 8000;
 const { loadPlanetsData } = require("./models/planets.model");
+// npm run watch
 
 const server = http.createServer(app);
 async function startServer() {
